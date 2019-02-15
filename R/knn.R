2949c59e51a54934e41b14f2e6314da2e9de4cd5
unknown <gec68@nau.edu>
2019-02-14 23:48:41 -0700

    Why are there so many packages needed to run a damn script? It makes zero sense and should already be implemented within the base packages that come included with RStudio. The amount of extra stuff we have to download is complete bullshit and I hate this ./endRant

--- R/knn.R
+++ R/knn.R
@@ -1,14 +1,27 @@
-#' Title
+#' K nearest neighbors alogrithm
 #'
-#' @param x.mat 
-#' @param y.vec 
-#' @param testx.vec 
-#' @param max.neighbors 
+#' An R function that wraps the nearest neighbor C++ code
+#' @param x.mat numeric train feature matrix [ n x p ]
+#' @param y.vec numeric train label vector [n], either all 
+#' 0/1 for binary classification, or other real numbers for
+#' regression (multi-class classification not supported).
+#' @param testx.vec numeric test feature vector [p]
+#' @param max.neighbors scalar integer, max number of neighbors
 #'
-#' @return
+#' @return numeric vector of size max.neighbors, predictions
+#' from 1 to max.neighbors.
 #' @export
 #'
 #' @examples
+#' data(zip.train, package = "ElemStatLearn")
+#' i01 <- which(zip.train[,1] %in% c(0,1))
+#' train.i <- i01[1:5]
+#' test.i <- i01[6]
+#' x <- zip.train[train.i, -1]
+#' y <- zip.train[train.i, 1]
+#' testx <- zip.train[test.i, -1]
+#' knn(x, y, testx, 3)
+#' zip.train[test.i]
 knn <- function(x.mat, y.vec, testx.vec, max.neighbors )
 {
   result.list <- .C("knn_interface",
