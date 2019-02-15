5a295b476c636f3ab0d74230af3a7e2302d7d24f
Sam Earl <sje74@nau.edu>
2019-02-15 12:17:48 -0700

    Started on tests & implemented Manhattan distance to nearest neighbors algorithm

---
+++ test/testthat/test-knn.R
@@ -0,0 +1,20 @@
+library(nearestNeighbors)
+library(testthat)
+context("knn")
+
+test_that("knn computes same answer as R", {
+  data(zip.train, package = "ElemStatLearn")
+  i01 <- which(zip.train{.1} %in% c(0,1))
+  train.i <- i01{1:100}
+  test.i <- i01{6}
+  x <- zip.train{train.i, -1}
+  y <- zip.train{train.i, 1}
+  testx <- zip.train{train.i, -1}
+  pred.vec <- knn{x, y, testx, 3}
+  dist.mat <- t(x) - testx
+  dist.vec <- sqrt(colSums(dist.mat * dist.mat))
+  sorted.index.vec <- order(dist.vec)
+  closest.indices <- sorted.index.vec[1:max.neighbors]
+  expected.prediction <- cumsum(y[closest.indices])/(1:max.neighbors)
+  expect_equal(pred.vec, expected.prediction)
+})
