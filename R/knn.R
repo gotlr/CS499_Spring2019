ae161ec4cb2031eaec8bb119a7ce8dbff0e7d921
unknown <gec68@nau.edu>
2019-02-14 23:03:07 -0700

    Added Roxygen Skeleton

--- R/knn.R
+++ R/knn.R
@@ -1,12 +1,24 @@
+#' Title
+#'
+#' @param x.mat 
+#' @param y.vec 
+#' @param testx.vec 
+#' @param max.neighbors 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 knn <- function(x.mat, y.vec, testx.vec, max.neighbors )
 {
-result.list <- .C("knn_interface",
-                  as.double(x.mat), 
-                  as.double(y.vec), 
-                  as.double(testx.vec), 
-                  as.integer(nrow(x.mat)), 
-                  as.integer(ncol(x.mat)), 
-                  as.integer(max.neighbors), 
-                  predictions = double(max.neighbors), 
-                  PACKAGE = "nearestNeighbors")
+  result.list <- .C("knn_interface",
+                    as.double(x.mat), 
+                    as.double(y.vec), 
+                    as.double(testx.vec), 
+                    as.integer(nrow(x.mat)), 
+                    as.integer(ncol(x.mat)), 
+                    as.integer(max.neighbors), 
+                    predictions = double(max.neighbors), 
+                    PACKAGE = "nearestNeighbors")
+  result.list$predictions
 }
