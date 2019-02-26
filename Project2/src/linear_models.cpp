5e7dce9db94e93fc9db2a2c7700b3dd0e542cafb
unknown <gec68@nau.edu>
2019-02-25 21:00:36 -0700

    Added prototype headers

--- Project2/src/linear_models.cpp
+++ Project2/src/linear_models.cpp
@@ -1,2 +1,37 @@
 #include "linear_models.h"
 
+int LMSquareLossIterations(
+  const int X.mat,
+  const int y.vec,
+  const int max.iterations,
+  const int step.size
+){
+  // Code
+}
+
+int LMLogisticLossIterations(
+    const int X.mat,
+    const int y.vec,
+    const int max.iterations,
+    const int step.size
+){
+  // Code
+}
+
+int LMSquareLossIterationsCV(
+    const int X.mat,
+    const int y.vec,
+    const int fold.vec,
+    const int max.iterations
+){
+  // Code
+}
+
+int LMLogisticLossIterationsCV(
+    const int X.mat,
+    const int y.vec,
+    const int fold.vec,
+    const int max.iterations
+){
+  // Code
+}
