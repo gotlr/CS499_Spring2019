da119ede24d475316dee3ee8708c578981ca9dc6
Sam Earl <sje74@nau.edu>
2019-03-07 20:57:34 -0700

    Started tests

---
+++ Project2/tests/testthat/test-lm.R
@@ -0,0 +1,11 @@
+library(linearModels)
+library(testthat)
+context("linear_models")
+
+test_that("linear models computes the same answer as R", {
+  data(zip.train, package = "ElemStatLearn")
+  #X.mat <-
+  #y.vec <-
+  #max.iterations <-
+  #test all functions in LMSquareLossIterations
+})
