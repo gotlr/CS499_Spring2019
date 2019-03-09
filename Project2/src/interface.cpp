65f2ef618402487dc5b93352b63ed252defeab1c
unknown <gec68@nau.edu>
2019-03-08 17:00:37 -0700

    Revert

--- Project2/src/interface.cpp
+++ Project2/src/interface.cpp
@@ -5,42 +5,54 @@
 void linear_models_interface(
     const double *X_mat,
     const double *y_vec,
-    const int n_observations,
-    const int n_features,
-    const int max_iterations,
-    const double step_size,
-    const double *w_mat
+    const int *n_observations,
+    const int *n_features,
+    const int *max_iterations,
+    const double *step_size,
+    double *weight_mat
 ){
   // Test LM__LossIterations
-    // Square Loss
-  if( max_iterations > 1 ){
-    int status = LMSquareLossIterations(
-        X_mat,
-        y_vec,
-        n_observations,
-        n_features,
-        max_iterations,
-        step_size,
-        w_mat);
-  }else{
-    error("ERROR: Max iterations needs to be greater than 1.");
+  // Square Loss
+  int status = LMSquareLossIterations(
+    X_mat,
+    y_vec,
+    *n_observations,
+    *n_features,
+    *max_iterations,
+    *step_size,
+    weight_mat);
+  
+  if( status != 0 ){
+    error("Non-zero exit status from LMSquareLossIterations");
   }
   
   
-    // Logistic Loss
-    
+  // Logistic Loss
+  status = LMSquareLossIterations(
+    X_mat,
+    y_vec,
+    *n_observations,
+    *n_features,
+    *max_iterations,
+    *step_size,
+    weight_mat);
+  
+  if( status != 0 ){
+    error("Non-zero exit status from LMLogisticLossIterations");
+  }
+  
   
   // Test LM__LossEarlyStoppingCV
-    // LMSquareLossEarlyStoppingCV
-    
-    // LMLogisticLossEarlyStoppingCV
+  // LMSquareLossEarlyStoppingCV
+  
+  // LMLogisticLossEarlyStoppingCV
 }
 
 // Package Information
 R_CMethodDef cMethods[] = 
   {
-    {"linear_models_interface", (DL_FUNC) &linear_models_interface, 6},
-    {NULL, NULL, 0}
+  {"linear_models_interface", (DL_FUNC) &linear_models_interface, 6},
+  {NULL, NULL, 0}
   };
 
 extern "C"{
--- Project2/src/linear_models.h
+++ Project2/src/linear_models.h
@@ -5,7 +5,7 @@ int LMSquareLossIterations(
     const int n_features,
     const int max_iterations,
     const double step_size,
-    const double *w_mat
+    double *weight_mat
 );
 
 int LMLogisticLossIterations(
